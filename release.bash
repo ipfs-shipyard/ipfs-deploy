61fc4a53baa3460236158a14141175ca3629f5d6
Agent of User <git@agentofuser.com>
2019-04-07 23:40:47 -0400

    chore: Automate pushing after release

--- release.bash
+++ release.bash
@@ -1,2 +1,2 @@
 #!/bin/sh
-export $(egrep -v '^#' .env | xargs) && node -r dotenv/config `which npx` semantic-release && rm .npmrc
+export $(egrep -v '^#' .env | xargs) && node -r dotenv/config `which npx` semantic-release && rm .npmrc && git push
