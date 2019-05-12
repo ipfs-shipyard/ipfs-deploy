98e55d475770040c3137816b1378aea76bc1c0d6
Oli Evans <oli@tableflip.io>
2019-05-12 18:41:37 +0100

    fix: return just gateway url if no cid provided
    License: MIT
    Signed-off-by: Oli Evans <oli@tableflip.io>

--- src/gateway.js
+++ src/gateway.js
@@ -1,13 +1,13 @@
 function getGatewayUrl(cid, gatewayProvider = 'ipfs') {
-  if (!cid) {
-    throw new Error('getGatewayUrl expects to be called with a valud IPFS CID')
-  }
   const gateways = {
     ipfs: 'https://ipfs.io',
     infura: 'https://ipfs.infura.io',
     pinata: 'https://gateway.pinata.cloud',
   }
   const origin = gateways[gatewayProvider] || gateways['ipfs']
+  if (!cid) {
+    return origin
+  }
   return `${origin}/ipfs/${cid}`
 }
 
