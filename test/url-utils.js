50b84758b207586734d922c6431b19ba228c607a
Marcin Rataj <lidel@lidel.org>
2019-09-06 11:08:26 +0200

    fix: return URL with trailing slash
    Removes the need for internal redirect done by gateway
    and closes #86 on gateways that override that behavior.
    
    License: MIT
    Signed-off-by: Marcin Rataj <lidel@lidel.org>

--- test/url-utils.js
+++ test/url-utils.js
@@ -3,19 +3,19 @@ const proxyquire = require('proxyquire').noCallThru()
 const { gatewayHttpUrl } = require('../src/url-utils')
 
 test('get http gateway url for a cid', t => {
-  const expected = 'https://ipfs.io/ipfs/fakecid'
+  const expected = 'https://ipfs.io/ipfs/fakecid/'
   const actual = gatewayHttpUrl('fakecid')
   t.is(actual, expected)
 })
 
 test('get http gateway url for a cid on infura', t => {
-  const expected = 'https://ipfs.infura.io/ipfs/fakecid'
+  const expected = 'https://ipfs.infura.io/ipfs/fakecid/'
   const actual = gatewayHttpUrl('fakecid', 'infura')
   t.is(actual, expected)
 })
 
 test('get http gateway url for a cid on pinata', t => {
-  const expected = 'https://gateway.pinata.cloud/ipfs/fakecid'
+  const expected = 'https://gateway.pinata.cloud/ipfs/fakecid/'
   const actual = gatewayHttpUrl('fakecid', 'pinata')
   t.is(actual, expected)
 })
