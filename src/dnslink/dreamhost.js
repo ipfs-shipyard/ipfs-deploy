089fb5ef8b201e0e7aed6dc330e49a5c1e6a0c4b
Ben Timby <btimby@gmail.com>
2021-02-27 01:41:17 -0500

    Linting.

--- src/dnslink/dreamhost.js
+++ src/dnslink/dreamhost.js
@@ -1,6 +1,5 @@
 const DreamHost = require('dreamhost')
 const _ = require('lodash')
-const fp = require('lodash/fp')
 
 module.exports = {
   name: 'DreamHost',
@@ -18,43 +17,42 @@ IPFS_DEPLOY_DREAMHOST__RECORD`)
     }
   },
   link: async (_domain, hash, { key, record }) => {
-    const link = `/ipfs/${hash}`;
+    const link = `/ipfs/${hash}`
 
     const options = {
-        type: 'TXT',
-        record: record,
-        value: link,
-        comment: 'Added by ipfs-deploy.',
-    };
-    const dreamHost = new DreamHost({ key });
-    const records = await dreamHost.dns.listRecords();
+      type: 'TXT',
+      record: record,
+      value: link,
+      comment: 'Added by ipfs-deploy.'
+    }
+    const dreamHost = new DreamHost({ key })
+    const records = await dreamHost.dns.listRecords()
     const forDomain = _.filter(records, (o) => {
-        return (o.record == options.record
-                && o.type == options.type
-                && o.value.startsWith('/ipfs/'));
-    });
+      return (o.record === options.record &&
+              o.type === options.type &&
+              o.value.startsWith('/ipfs/'))
+    })
     _.each(forDomain, async (o) => {
-        await dreamHost.dns.removeRecord({
-            type: o.type,
-            record: o.record,
-            value: o.value
-        });
-    });
+      await dreamHost.dns.removeRecord({
+        type: o.type,
+        record: o.record,
+        value: o.value
+      })
+    })
 
     // Sometimes the deletes take a little while to settle.
     return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            dreamHost.dns
-                .addRecord(options)
-                .then(() => {
-                    resolve({
-                        record: record,
-                        value: options.value
-                    });
-                })
-                .catch(reject);
-        }, 100);
-    });
-
+      setTimeout(() => {
+        dreamHost.dns
+          .addRecord(options)
+          .then(() => {
+            resolve({
+              record: record,
+              value: options.value
+            })
+          })
+          .catch(reject)
+      }, 100)
+    })
   }
 }
