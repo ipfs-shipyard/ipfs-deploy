44c7ac6a4782004717d901fb6eb3ec967c3e597f
Chris Cowan <agentme49@gmail.com>
2023-02-04 15:57:13 -0800

    Re-enable and update @filebase/client
    Issue that lead to it being disabled has been fixed:
    https://github.com/filebase/filebase-js/issues/3

--- src/pinners/index.js
+++ src/pinners/index.js
@@ -3,16 +3,15 @@ import Infura from './infura.js'
 import IpfsCluster from './ipfs-cluster.js'
 import Pinata from './pinata.js'
 import c4rex from './c4rex.js'
-// Disabled for now as '@filebase/client' dependency doesn't work in ESM mode yet.
-// import Filebase from './filebase.js'
+import Filebase from './filebase.js'
 
 export const pinners = [
   DAppNode,
   Infura,
   IpfsCluster,
   Pinata,
-  c4rex
-  // Filebase
+  c4rex,
+  Filebase
 ]
 
 export const pinnersMap = pinners.reduce((map, pinner) => {
